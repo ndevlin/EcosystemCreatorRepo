bd28311a724ddf8be80d6ca97f41c39ad86d7f45
Alexis Ward <aleward@seas.upenn.edu>
2021-05-02 16:03:36 -0400

    Still fixing transforms

--- EcosystemCreator/SOP_Branch.C
+++ EcosystemCreator/SOP_Branch.C
@@ -282,7 +282,7 @@ SOP_Branch::cookMySop(OP_Context &context)
 
 			GU_AgentLayerConstPtr currLayer = ptrTemp->layer(UTmakeUnsafeRef(GU_AGENT_LAYER_DEFAULT));
 			moduleAgent->setCurrentLayer(packedPrim, currLayer);
-			setTransforms(root);
+			//setTransforms(root);
 			std::cout << "Reached5" << std::endl;
 
 			//moduleAgent = UTverify_cast<GU_Agent*>(packedPrim->hardenImplementation());
